d855c892f9dc4bc0154b8cf0008d0c7ade626811
lukoda <dennis.lupiana@gmail.com>
2026-09-09 23:08:35 +0300

    Update SubjectController.php

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -267,6 +267,8 @@ public function index(Request $request)
     $combinations = Combination::whereDoesntHave('schools', function ($query) use ($schoolId) {
         $query->where('school_id', $schoolId);
     })->get();
+
+    dd($subjects);
     return view('subjects.list', compact('subjects', 'school', 'combinations'));
 }
     
