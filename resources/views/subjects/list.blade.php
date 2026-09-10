b7039e55d131c842874cc85c0b1dfb2a10a5722f
lukoda <dennis.lupiana@gmail.com>
2026-09-10 13:56:32 +0300

    Update list.blade.php

--- resources/views/subjects/list.blade.php
+++ resources/views/subjects/list.blade.php
@@ -75,7 +75,7 @@
 										<i class="fas fa-plus"></i> Add Subject
 									</a>
 
-									@if(!empty($school->subjects()->get()) && count($school->subjects()->get()) > 0)
+									@if(!empty($school->subjects()) && count($school->subjects()) > 0)
 										<button type="button" class="mt-3 mr-3 btn btn-info" data-toggle="modal" data-target="#editSubjectModal">
 											<i class="fas fa-edit"></i> Edit Subject
 										</button>
