eefb9a291b3c8df9af6dfd471d6e77ce3a23b250
lukoda <dennis.lupiana@gmail.com>
2026-09-10 20:32:47 +0300

    Update list.blade.php

--- resources/views/subjects/list.blade.php
+++ resources/views/subjects/list.blade.php
@@ -283,18 +283,26 @@
 						<div class="modal-body">
 							<div class="alert alert-warning">
 								<i class="fas fa-exclamation-triangle"></i> 
-								<span class="text-small"><strong>Warning:</strong> This will remove the subject from the school.</span>
+								<span class="text-small"><strong>Warning:</strong> This will remove the selected subject(s) from the school.</span>
 							</div>
 							
-							<div class="form-group">
-								<label>Select Subject</label>
-								<select class="form-control select2" id="delete_subject_id" name="subject_id" required style="width: 100%;">
-									<option value="" selected disabled>-- Choose Subject --</option>
-									@foreach($school->subjects()->get() as $subject)
-										<option value="{{ $subject->id }}">{{ $subject->name }}</option>
-									@endforeach
-								</select>
-							</div>
+<div class="form-group">
+    <label class="d-block">Select Subjects</label>
+    
+    @foreach($school->subjects()->get() as $subject)
+        <div class="form-check form-check-inline mb-2">
+            <input class="form-check-input" 
+                   type="checkbox" 
+                   name="subject_id[]" 
+                   id="subject_{{ $subject->id }}" 
+                   value="{{ $subject->id }}">
+            <label class="form-check-input-label ml-1" for="subject_{{ $subject->id }}">
+                {{ $subject->name }}
+            </label>
+        </div>
+    @endforeach
+</div>
+
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
