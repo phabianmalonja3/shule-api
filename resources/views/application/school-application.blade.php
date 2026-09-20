20a76f19f1c2b562a7adc0f91b61ca34f2d3c3ef
lukoda <dennis.lupiana@gmail.com>
2026-09-20 22:31:02 +0300

    Update school-application.blade.php

--- resources/views/application/school-application.blade.php
+++ resources/views/application/school-application.blade.php
@@ -108,18 +108,21 @@
                                         </div>
 <!-- O-Level Combinations Container -->
 <div class="o-level-combinations-container col-12 mt-3" style="display: none;">
-    <div class="form-group mb-0 p-3 bg-light rounded">
+    <div class="form-group mb-0 p-3 bg-light rounded border">
         <label class="font-weight-bold mb-2 text-dark">O-Level Combinations</label>
         <div class="row pl-2">
             @foreach($combinations as $combination)
                 @if($combination->level == 'O-Level' && $combination->id != 1)
                     <div class="col-md-3 col-sm-4 col-6 mb-2">
-                        <div class="form-check">
+                        <div class="form-check d-flex align-items-center">
                             <input type="checkbox" 
                                    name="schools[0][o_level_combinations][]" 
                                    id="o_comb_0_{{ $combination->id }}" 
                                    value="{{ $combination->id }}">
-                            <label class="form-check-label" for="o_comb_0_{{ $combination->id }}">
+                            <label class="form-check-label text-truncate" 
+                                   for="o_comb_0_{{ $combination->id }}"
+                                   title="{{ $combination->name }}"
+                                   style="max-width: 100%; cursor: pointer;">
                                 {{ $combination->name }}
                             </label>
                         </div>
