e03aabae6e90bf1146dbbf3b6b214214c7619c8f
lukoda <dennis.lupiana@gmail.com>
2026-09-21 17:23:03 +0300

    Update school-list.blade.php

--- resources/views/livewire/school/school-list.blade.php
+++ resources/views/livewire/school/school-list.blade.php
@@ -3,7 +3,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h4>List of Registered Schools</h4>
+                    <h4>Registered Schools</h4>
                     <div class="row">
 
                         <div class="d-flex justify-content-between align-items-center">
@@ -46,7 +46,7 @@
 
                                     <th>#</th>
                                     <th>School Name</th>
-                                    <th>Level</th>
+                                    <th>Level(s)</th>
                                     <th>Sponsorship</th>
                                     <th>Headteacher</th>
 
