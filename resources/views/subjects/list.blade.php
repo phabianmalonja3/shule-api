9b0ed79eb963520e0e3105a88cbaa90a12229ce2
lukoda <dennis.lupiana@gmail.com>
2026-09-21 23:54:29 +0300

    Update list.blade.php

--- resources/views/subjects/list.blade.php
+++ resources/views/subjects/list.blade.php
@@ -182,12 +182,7 @@
 						<div class="modal-body">
 							<div class="form-group">
 								<label>Select Combination</label>
-								<select class="form-control select2" id="edit_combination_id" name="combination_id" required style="width: 100%;">
-									<option value="" selected disabled>-- Choose Combination --</option>
-									@foreach($school->combinations as $comb)
-										<option value="{{ $comb->id }}">{{ $comb->name }}</option>
-									@endforeach
-								</select>
+
 							</div>
 
 							<div class="form-group">
