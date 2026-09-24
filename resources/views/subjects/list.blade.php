27f8b9d702f09eebfa485cf322ec8a86e96d3a1c
lukoda <dennis.lupiana@gmail.com>
2026-09-25 00:53:06 +0300

    Update list.blade.php

--- resources/views/subjects/list.blade.php
+++ resources/views/subjects/list.blade.php
@@ -155,8 +155,8 @@
 							<div class="form-group">
 								<label for="subjects_list">Select Subjects</label>
 								<select class="form-control ss-select-tags" id="subjects_list" name="subjects[]" multiple="multiple" style="width: 100%; height: 50%">
-									@isset($subjects)
-										@foreach($subjects as $subject)
+									@isset($allSubjects)
+										@foreach($allSubjects as $subject)
 											<option value="{{ $subject->id }}">{{ $subject->name }}</option>
 										@endforeach
 									@else
