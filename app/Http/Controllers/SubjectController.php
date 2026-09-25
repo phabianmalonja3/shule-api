a676673adf018f2c14bfbc431710ae19fe26d0b7
lukoda <dennis.lupiana@gmail.com>
2026-09-25 23:59:31 +0300

    dd

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -542,6 +542,9 @@ foreach ($combinations as $combination) {
     ];
 
 }
+
+$x = array_values($extraGeneralSubjectNames);
+return $x;
     return view('subjects.list', compact(
         'subjects',                // Display Grid (Active combination subjects + Direct school subjects)
         'allSubjects',             // Modal Dropdown (All level subjects + Direct school subjects)
--- resources/views/subjects/list.blade.php
+++ resources/views/subjects/list.blade.php
@@ -67,7 +67,7 @@
     <label class="form-check-label text-truncate" 
            title="{{ $subjectName }}"
            style="max-width: 100%; cursor: pointer;">
-        {{ $subjectName }} {{ count($extraGeneralSubjectNames) }}
+        {{ $subjectName }}
     </label>
   
                     </div>
